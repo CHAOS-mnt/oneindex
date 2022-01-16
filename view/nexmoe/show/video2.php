0c7bc091b1d04a1f4a2e9c28e7183f786003afae
CHAOS-mnt <114514@yjsnpi.monster>
2022-01-16 10:17:04 +0900

    h5, DASH播放页面优化

--- view/nexmoe/show/video2.php
+++ view/nexmoe/show/video2.php
@@ -16,15 +16,27 @@ $mpd =  str_replace("thumbnail","videomanifest",$item['thumb'])."&part=index&for
 <div class="mdui-container-fluid">
 	<div class="nexmoe-item">
 	<div class="mdui-center" id="dplayer"></div>
+	<div class="mdui-p-t-5 ">
+		<button id="appplayers" class="mdui-btn mdui-ripple mdui-color-theme-accent">外部播放器播放</button>		
+		<ul class="mdui-menu mdui-menu-cascade" id="menu">
+			<li class="mdui-menu-item">
+			<a href="intent:<?php e($url);?>;end" class="mdui-ripple">MXPlayer(FREE)</a>
+			</li>
+			<li class="mdui-menu-item">
+			<a href="vlc://<?php e($url);?>" class="mdui-ripple">VLC(PC)</a>
+			</li>
+			<li class="mdui-menu-item">
+			<a href="potplayer://<?php e($url);?>" class="mdui-ripple">PotPlayer(PC)</a>
+			</li>
+			<li class="mdui-menu-item">
+			<a href="nplayer-<?php e($url);?>" class="mdui-ripple">nPlayer(ios)</a>
+			</li>
+		</ul>
+	</div>
 	<!-- 固定标签 -->
 	<div class="mdui-textfield">
 	  <label class="mdui-textfield-label">下载地址</label>
 	  <input class="mdui-textfield-input" type="text" value="<?php e($url);?>"/>
-	</div>
-	<div class="mdui-textfield">
-	  <label class="mdui-textfield-label">引用地址</label>
-	  <textarea class="mdui-textfield-input"><video><source src="<?php e($url);?>" type="video/mp4"></video></textarea>
-	</div>
 	</div>
 </div>
 <script>
--- view/nexmoe/show/video5.php
+++ view/nexmoe/show/video5.php
@@ -8,7 +8,7 @@ $item['thumb'] = onedrive::thumbnail($item['path']);
 <div class="mdui-container-fluid">
 	<div class="nexmoe-item">
 	<video class="mdui-video-fluid mdui-center" preload controls poster="<?php @e($item['thumb']);?>">
-	  <source src="<?php e($item['downloadUrl']);?>" type="video/mp4">
+	  <source src="<?php e($item['downloadUrl']);?>" type="video/mkv">
 	</video>
 	<div class="mdui-p-t-5 ">
 		<button id="appplayers" class="mdui-btn mdui-ripple mdui-color-theme-accent">外部播放器播放</button>		
@@ -25,7 +25,6 @@ $item['thumb'] = onedrive::thumbnail($item['path']);
 			<li class="mdui-menu-item">
 			<a href="nplayer-<?php e($url);?>" class="mdui-ripple">nPlayer(ios)</a>
 			</li>
-
 		</ul>
 	</div>
 	<!-- 固定标签 -->
